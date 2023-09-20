0f660499c43265679382db82be5ce9ea6c6e6440
Sabarinathan097 <145537946+Sabarinathan097@users.noreply.github.com>
2023-09-20 13:27:26 +0530

    initial commit

---
+++ ReverseStrings.java
@@ -0,0 +1,23 @@
+public class Reverse_String {
+	
+	public static void main(String[] args) {
+		
+		String name = "Arun";
+		
+		String reverse="";
+		
+		for (int i = name.length()-1; i>=0; i--) {
+			
+			//int i=3; 3>=0;
+			//int i=2; 2>=0;
+			//int i=1; 1>=0;
+			//int i=0; 0>=0;
+			
+			reverse = reverse+name.charAt(i);
+			
+		}
+		
+		System.out.println(reverse);
+	}
+
+}
